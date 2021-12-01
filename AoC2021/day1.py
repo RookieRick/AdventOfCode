cfc1b61a75fb6a7e0c5077106311c34367e3dacd
Rick Riensche <rick.riensche@autodesk.com>
2021-11-30 21:14:38 -0800

    day 1 2021

--- AoC2021/day1.py
+++ AoC2021/day1.py
@@ -2,4 +2,22 @@ import AdventOfCode.util.input_parser as parser
 
 
 if __name__=="__main__":
-    data = parser.parse("./raw_inputs/day1.txt", transforms=[parser.Split('\n'), parser.Split('\n\n')], blob=True)
+    data = parser.parse("./raw_inputs/day1.txt", transforms=[parser.Cast(int)])
+
+    # part 1
+    increases = 0
+    for i in range(1, len(data)):
+        if data[i] > data[i-1]:
+            increases += 1
+    print(increases)
+
+    # part 2
+
+    increases = 0
+    for i in range(3, len(data)):
+        if sum(data[i-3:i]) > sum(data[i-4:i-1]):
+            increases += 1
+
+    print(increases)
+
+

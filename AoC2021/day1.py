f4c5a889f17efc152f640aa08566d4dd3469524a
Rick Riensche <rick.riensche@autodesk.com>
2021-12-08 19:46:46 -0800

    formatting

--- AoC2021/day1.py
+++ AoC2021/day1.py
@@ -1,7 +1,7 @@
 import AdventOfCode.util.input_parser as parser
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     data = parser.parse("./raw_inputs/day1.txt", transforms=[parser.Cast(int)])
 
     # part 1

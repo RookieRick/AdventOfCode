436139be6c26a2cd4ded820b4488ac6de2475bce
Rick Riensche <rookierick@rookierick.com>
2023-12-04 20:54:13 -0800

    Create a template so adding a new day is just mkdir+cp

---
+++ Golang/cmd/2023/day/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"rookierick.com/aoc/internal/utils/inputs"
+)
+
+func main() {
+	input, err := inputs.GetInput()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(input))
+
+	for _, line := range input {
+
+	}
+
+	fmt.Println("fin.")
+
+}

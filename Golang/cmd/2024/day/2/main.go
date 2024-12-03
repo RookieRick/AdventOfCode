e04494849e62d8ceafe1ce4b36ac9b3e77ba093d
Rick Riensche <rookierick@rookierick.com>
2024-12-02 20:17:02 -0800

    Cleaner approach overall and semi-brute part 2

--- Golang/cmd/2024/day/2/main.go
+++ Golang/cmd/2024/day/2/main.go
@@ -8,6 +8,8 @@ import (
 	"rookierick.com/aoc/internal/utils/inputs"
 )
 
+const debug = false
+
 func main() {
 	input, err := inputs.GetInput()
 	if err != nil {
@@ -28,52 +30,26 @@ func main() {
 	var unsafeReports [][]int
 	safeCount := 0
 	for _, report := range reports {
-		// first determine asc/desc order and select appropriate "safety check" func
-		firstDiff := report[0] - report[1]
-		var unsafeDiff func(int) bool
-		if firstDiff == 0 {
-			unsafeReports = append(unsafeReports, report)
-			continue // abs(diff) must be at least 1
-		}
-
-		if firstDiff > 0 {
-			unsafeDiff = unsafeDiffDescending
-		} else {
-			unsafeDiff = unsafeDiffAscending
-		}
-		if unsafeDiff(firstDiff) {
-			unsafeReports = append(unsafeReports, report)
-			continue // diff not between 1 and 3 inclusive
-		}
-		// now check remainder of report values:
-		safe := true
-		for i := 1; i < len(report)-1; i++ {
-			diff := report[i] - report[i+1]
-			if unsafeDiff(diff) {
-				safe = false
-				unsafeReports = append(unsafeReports, report)
-				break
-			}
-		}
-		if safe {
+		if isSafeReport(report) {
 			safeCount++
+		} else {
+			unsafeReports = append(unsafeReports, report)
 		}
 	}
 	fmt.Println("Part 1 safe count: ", safeCount)
 
 	// part 2 - take another pass through those captured as unsafe to see if they can be salvaged
+	preScreened := 0 // counter to see how many we can reject outright for having more than 1 "unsafe" delta
 
 reportLoop: // label the outer loop so we can jump to next report if we find a safe variant.
-	// slightly different approach now since we can't assume first pair indicates asc/desc..
-	// (prob should have done it this way initially?) --> maybe rewrite the part that evaluates sequence as a func
-	//   accepting an argument for max # of entries to remove..  (0 for part 1, 1 for part 2)
-	//  Keep the "pre-rejection" stuff to winnow the field down before you go slightly more iterative..
 	for _, report := range unsafeReports {
 		var deltas []int
 		var negatives, positives, zeros int
 
 		dump := func() {
-			fmt.Println("report: ", report, "deltas:", deltas)
+			if debug {
+				fmt.Println("report: ", report, "deltas:", deltas)
+			}
 		}
 
 		for i := 0; i < len(report)-1; i++ {
@@ -90,16 +66,28 @@ reportLoop: // label the outer loop so we can jump to next report if we find a s
 			if zeros > 0 {
 				if zeros > 1 || (positives > 0 && negatives > 0) { // multiple zero deltas or a zero and both pos and neg..
 					dump()
+					preScreened++
 					continue reportLoop
 				}
 			} else if (positives > 1 && negatives+zeros > 1) || (negatives > 1 && positives+zeros > 1) {
-				//dump()
+				dump()
+				preScreened++
 				continue reportLoop
 			}
 		}
 		// if we made it this far, we haven't ruled this one out as having multiple zeros or more than 1 mismatched delta type..
-
+		// we could probably be clever here about how we remove and retry, but in the interest of time I'll just brute force it:
+		for skip := range len(report) {
+			modifiedReport := make([]int, 0)
+			modifiedReport = append(modifiedReport, report[:skip]...)
+			modifiedReport = append(modifiedReport, report[skip+1:]...)
+			if isSafeReport(modifiedReport) {
+				safeCount++
+				continue reportLoop
+			}
+		}
 	}
+	fmt.Printf("Rejected %v of %v reports in prescreening\n", preScreened, len(unsafeReports))
 
 	fmt.Println("Part 2 safe count:", safeCount)
 	fmt.Println("fin.")
@@ -113,10 +101,19 @@ func abs(x int) int {
 	return x
 }
 
-func unsafeDiffAscending(diff int) bool {
-	return diff >= 0 || abs(diff) > 3
-}
-
-func unsafeDiffDescending(diff int) bool {
-	return diff <= 0 || abs(diff) > 3
+func isSafeReport(levels []int) bool {
+	lastDelta := 0 // negative if delta was in descending order, positive if ascending, 0 if equal (unsafe)
+	for i := range len(levels) - 1 {
+		delta := levels[i+1] - levels[i]
+		if delta == 0 || abs(delta) > 3 {
+			// violated constraint of delta between 1..3
+			return false
+		}
+		if (delta < 0 && lastDelta > 0) || (delta > 0 && lastDelta < 0) {
+			// violated all ascending/descending constraint
+			return false
+		}
+		lastDelta = delta
+	}
+	return true
 }
